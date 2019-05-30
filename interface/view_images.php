b7969043d7204ba57c4c1e8bf40beaa408360730
IliyanTachev <iliqn.tachev@gmail.com>
2019-05-30 22:09:11 +0300

    update code design

--- interface/view_images.php
+++ interface/view_images.php
@@ -1,5 +1,4 @@
 <?php 
-	//include 'login.php';
 	include 'head.php';
 	include 'navbar-top.php';
 	include 'nav-side-menu.php';
@@ -15,9 +14,9 @@
 ?>
 <html>
 	<body>
-			<div class="contentGallery col-10" style="position: relative;">
-				<div class="baguetteBoxOne galleryImgs">
-				<?php 
+		<div class="contentGallery col-10" style="position: relative;">
+			<div class="baguetteBoxOne galleryImgs">
+			<?php 
 				$sql = "SELECT * FROM media_files";
 				$res = mysqli_query($conn, $sql);
 				if(mysqli_num_rows($res) == 0){ ?>
@@ -32,9 +31,9 @@
 						$imgPath = $root_dir."uploads/images/".$row['file_name'];
 						$imageSRC = $media_sources . "images/" . $row['file_name'];
 						if(file_exists($imgPath)){ ?>
-								  <a href="<?php echo $imageSRC;?>" class="lightbox" style="text-align: center;">
-								 	  <img src="<?php echo $imageSRC;?>" class="gallery-images" alt="Image unavailable" name="<?php echo $row['file_name'];?>">  
-									</a>
+						    <a href="<?php echo $imageSRC;?>" class="lightbox" style="text-align: center;">
+							 	  <img src="<?php echo $imageSRC;?>" class="gallery-images" alt="Image unavailable" name="<?php echo $row['file_name'];?>">  
+							</a>
 									<!--<div class="buttonImgShow"><a href="view_images.php?cmd=show&ID=<?php echo $row['ID']; ?>"> SHOW </a> -->
 						<?php 
 						} 
@@ -42,38 +41,22 @@
 					}
 				} ?>
 					
-					</div>
-				</div>
-			<script>
-	    		window.onload = function() {
-	        	baguetteBox.run('.baguetteBoxOne');
-	   			};
-			</script>
-
-			<div class="uploader" style="display: none;">
-				<form id="upload_file" enctype="multipart/form-data" method="post" action="uploader.php">
-					<input type="file" name="file" id="fileInput" />
-					<input type="hidden" name="type" value="1" /> 
-				</form>
 			</div>
+		</div>
+		<script>
+	    	window.onload = function() {
+	       	baguetteBox.run('.baguetteBoxOne');
+	   		};
+		</script>
+
+		<div class="uploader" style="display: none;">
+			<form id="upload_file" enctype="multipart/form-data" method="post" action="uploader.php">
+				<input type="file" name="file" id="fileInput" />
+				<input type="hidden" name="type" value="1" /> 
+			</form>
+		</div>
 	</body>
 </html>
-<script>
-	$(document).ready(function(){
-  	/*$( window ).resize(function() {
-			if($( document ).width() <= 991){
-				$('.contentGallery').css("left", "0");
-				$('.contentGallery').addClass("container");
-				$('.galleryImgs').css("text-align", "center");
-			}
-			else if($( document ).width() >= 992) {
-				$('.contentGallery').css({"position":"relative", "left":"260"});
-				$('.contentGallery').removeClass("container");
-				$('.galleryImgs').css("text-align", "");
-			}
-		});*/
-  });
-</script>
 <?php
 	function getExtension($fileName){
 		$nameToArray = explode('.', $fileName);
