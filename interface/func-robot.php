58377e604ceedf50356bb2234de3d2dba0dfe171
IliyanTachev <iliqn.tachev@gmail.com>
2019-05-30 21:59:02 +0300

    changed show_emoji and added emoji

--- interface/func-robot.php
+++ interface/func-robot.php
@@ -2,30 +2,6 @@
 include 'db_con.php';
 include 'config.php';
 
-if($_GET['func'] == "stop"){
-	emergency_stop();
-}
-else if($_POST['func'] == "move"){
-	$lin_speed = $_POST['lin_speed'];
-	move($lin_speed);
-}
-
-function emergency_stop(){
-	$cmd = "pidof omxplayer.bin";
-	$result = exec($cmd);
-	//echo "result: " . $result;
-	$tmp = "kill -15 " . $result; 
-	$res = exec($tmp);
-	//echo "$tmp";
-	//var_dump($res);
-}
-
-function init(){
-	$cmds=array("roslaunch turtlebot3_bringup bebot.launch", "roslaunch rosbridge_server rosbridge_websocket.launch");
-	foreach($cmds as $cmd){
-		var_dump(exec($cmd));
-	}	
-}
 
 function say($text="1 2 3",$lang="bg",$pitch=10,$gap=7,$speed=140)
 { 
@@ -38,7 +14,6 @@ function say($text="1 2 3",$lang="bg",$pitch=10,$gap=7,$speed=140)
  if ($debug) echo($cmd);
 }
 
-// Beeps from Turtlebot Buzzer - Requires OpenCR board
 function turtlebot_sound($param=0)
 {
 	global $debug;
@@ -47,16 +22,34 @@ function turtlebot_sound($param=0)
   if ($debug) echo($cmd);
 }
 
-function show_emoji($param=0)
+function emoji($param="усмихнат",$time=1)
 {
-	global $debug;
-	$cmd="sudo /usr/bin/fbi -d /dev/fb0 -T 1 -a -noverbose -nocomments /home/pi/emotics/emoji_".(int)$param.".png"; 
-	exec($cmd);
+  global $debug,$PT,$allem;
+  $emoji=$allem[$param];
+    $ptype=4;
+    $cmd="sudo /usr/bin/fbi -d /dev/fb0 -1 -t $time -T 1 -a -noverbose -nocomments /home/pi/emotics/emoji_".(int)$emoji.".png"; 
+    exec($cmd);
   if ($debug) echo($cmd);
 }
 
+function show_image($ID, $apect="stretch")
+{
+  global $debug, $conn, $turtlebot_files_dir;
+	$ID= (int)$ID;
+	$sql = "SELECT file_name FROM media_files WHERE ID='$ID' AND content_type=1 LIMIT 1";
+  $res = mysqli_query($conn, $sql);
+  if(mysqli_num_rows($res) > 0){
+  	$row = mysqli_fetch_assoc($res);
+  	$filename = $row['file_name'];
+  	$cmd="sudo /usr/bin/fbi -d /dev/fb0 -T 1 -a -noverbose -nocomments \"$turtlebot_files_dir/uploads/images/$filename\"";
+	  if (!$filename) $cmd="sudo /var/www/html/libs/clr_frame.sh";
+	  exec($cmd);
+	  if ($debug) echo($cmd);
+  }
+  return false;
+}
+
 //play video or audio file
-//function play_media($filename,$aspect="stretch")
 function play_media($ID, $aspect="stretch")
 {
 	global $debug, $conn, $turtlebot_files_dir;
@@ -71,8 +64,6 @@ function play_media($ID, $aspect="stretch")
   	$split_filename = explode(".", $filename);
   	if(end($split_filename) == "mp3") $media_dir = "audios";
   	else if(end($split_filename) == "mp4") $media_dir = "videos";
-  	//$pid="/usr/bin/omxplayer --aspect $aspect  \"$turtlebot_files_dir/uploads/$media_dir/$filename\" ";
-	  //$pid = exec("sudo /usr/bin/omxplayer --aspect $aspect  \"$filename\" ");//  > /dev/null 2>&1 & echo $!");
 	  $pid = exec("sudo /usr/bin/omxplayer --aspect $aspect  \"$turtlebot_files_dir/uploads/$media_dir/$filename\" ");//  > /dev/null 2>&1 & echo $!");
 	  var_dump ($pid);
 	  ///$pid=exec($cmd);
